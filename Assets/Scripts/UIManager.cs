a70fbb8c3b2970760203d30bacb1023f45db7eea
Maarten Broekman <maarten@broekman.us>
2021-06-14 17:32:03 -0400

    Update UIManager.cs
    No need for the init functions on the text elements. Added counter for player lives. Added world speed for increase world velocity.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,24 +8,48 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _coinCounter;
 
+    [SerializeField]
+    private Text _livesCounter;
+
+    [SerializeField]
+    private float _worldSpeed;
+
     void Start()
     {
-        _coinCounter.text = "Coins: ";
-
-        InitCoins();
+        _coinCounter = GameObject.Find("Canvas/Coin_text").GetComponent<Text>();
+        if (_coinCounter != null)
+        {
+            _coinCounter.text = "Coins: ";
+        }
+        _livesCounter = GameObject.Find("Canvas/Lives_text").GetComponent<Text>();
+        if (_livesCounter != null)
+        {
+            _livesCounter.text = "Lives: ";
+        }
+        _worldSpeed = 1f;
     }
 
     private void Update()
     {
     }
 
-    private void InitCoins()
-    {
-        SetCoins(0);
-    }
-
     public void SetCoins(int coins)
     {
         _coinCounter.text = "Coins: " + coins.ToString();
+    }
+
+    public void SetLives(int lives)
+    {
+        _livesCounter.text = "Lives: " + lives.ToString();
+    }
+
+    public void SetWorldSpeed(float newSpeed)
+    {
+        _worldSpeed = newSpeed;
+    }
+
+    public float GetWorldSpeed()
+    {
+        return _worldSpeed;
     }
 }
