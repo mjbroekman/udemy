c2e3823ad09967293e3cc36cae60f8a750c7236c
Maarten Broekman <maarten@broekman.us>
2021-06-19 16:09:31 -0400

    Update Player.cs
    setting speed during field assignment results in speed being 0 after program start (?!).

--- FPSDemo/Assets/Game/Scripts/Player.cs
+++ FPSDemo/Assets/Game/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     private CharacterController _controller;
 
     [SerializeField]
-    private float _speed = 3.5f;
+    private float _speed;
 
     [SerializeField]
     private float _gravity = 9.81f;
@@ -18,7 +18,9 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        _speed = 3.5f;
         _controller = GetComponent<CharacterController>();
+        _velocity = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -27,16 +29,15 @@ public class Player : MonoBehaviour
         // Get user input for the direction of movement
         foreach (KeyCode kcode in System.Enum.GetValues(typeof(KeyCode)))
         {
-            if (Input.GetKey(kcode))
-                Debug.Log("KeyCode down: " + kcode);
+            if (Input.GetKey(kcode)) Debug.Log("KeyCode down: " + kcode);
         }
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         
         // Apply speed to direction to get velocity
         _velocity = direction * _speed;
 
         // Apply gravity to y-component of velocity
-        // _velocity.y -= _gravity;
+        if (!_controller.isGrounded) _velocity.y -= _gravity;
 
         // Move the player based on how much time has passed since the last update
         _controller.Move(_velocity * Time.deltaTime);
